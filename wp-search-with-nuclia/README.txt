24480f9ec67d84f39daf510c3f1e723a8adad2a4
Serge Rauber <contact@kalyx.fr>
2023-02-10 15:43:37 +0100

    Add files via upload

---
+++ wp-search-with-nuclia/README.txt
@@ -0,0 +1,72 @@
+=== WP Search With Nuclia ===
+Contributors: Kalyx
+Tags: Search, Nuclia, relevant search, search highlight, faceted search, search by category, ajax search, better search, custom search
+Requires at least: 5.0
+Tested up to: 6.1.1
+Requires PHP: 7.2
+Stable tag: 1.0.0
+License: GNU General Public License v2.0, MIT License
+
+Improve search on your site.
+
+== Description ==
+
+Improve search on your site.
+
+= Features =
+* Relevant search results in milliseconds
+* Native typo-tolerance
+* Is language-agnostic
+* Offers hooks and filters for easy customization
+* Built by developers, for developers
+
+This plugin requires a Nuclia account. You can sign up for a free trial at [Nuclia](https://nuclia.com).
+
+= Links =
+* [Nuclia](https://nuclia.com)
+
+
+*This plugin is a derivative work of the code from the [WP Search With Nuclia](https://wordpress.org/plugins/wp-search-with-nuclia/) plugin for WordPress, which is licensed under the GPLv2.*
+
+== Installation ==
+
+
+From your WordPress dashboard:
+
+1. **Visit** Plugins > Add New
+2. **Search** for "WP Search With Nuclia"
+3. **Activate** WP Search With Nuclia from your Plugins page
+4. **Click** on the new menu item "Nuclia Search" and enter your API keys
+5. **Read** the step by step [configuration guide](https://github.com/WebDevStudios/wp-search-with-nuclia/wiki/Getting-Started)
+
+== Frequently Asked Questions ==
+
+= Is this plugin a fork? =
+
+Yes. 
+
+= What are the minimum requirements? =
+
+* Requires WordPress 5.0+
+* PHP version 7.2 or greater (PHP 7.3 is recommended)
+* MySQL version 5.0 or greater (MySQL 5.6 or greater is recommended)
+* cURL PHP extension
+* mbstring PHP extension
+* OpenSSL greater than 1.0.1
+
+= Will it work with my theme? =
+
+Yes. This plugin should work with most themes that do not override the default WordPress search behavior. Instant Search results page may require some styling to make it match nicely. See the [Search Page Customization](https://github.com/WebDevStudios/wp-search-with-nuclia/wiki/Customize-your-search-page).
+
+= Where can I report bugs, request features, or contribute to the project? =
+
+
+= About Nuclia =
+
+Nuclia offers its Search as a Service provider on a incremental payment program, including a free Community Plan which includes 10,000 records & 50,000 operations per month. Beyond that, [plans](https://www.nuclia.com/pricing/) start at $29/month.
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release.
---
+++ wp-search-with-nuclia/nuclia.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Plugin Name:       WP Search With Nuclia
+ * Plugin URI:        https://kalyx.fr
+ * Description:       Integrate the powerful Nuclia search service with WordPress
+ * Version:           1.0.0
+ * Requires at least: 5.6
+ * Requires PHP:      7.2
+ * Author:            Serge Rauber
+ * Author URI:        https://kalyx.fr
+ * License:           GNU General Public License v2.0 / MIT License
+ * Text Domain:       wp-search-with-nuclia
+ * Domain Path:       /languages
+ *
+ * @since   1.0.0
+ * @package Kalyx\WPSWN
+ */
+
+// Nothing to see here if not loaded in WP context.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// The Nuclia Search plugin version.
+define( 'NUCLIA_VERSION', '1.0.0' );
+
+// The minmum required PHP version.
+define( 'NUCLIA_MIN_PHP_VERSION', '7.2' );
+
+// The minimum required WordPress version.
+define( 'NUCLIA_MIN_WP_VERSION', '5.6' );
+
+define( 'NUCLIA_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+
+define( 'NUCLIA_PLUGIN_URL', plugins_url( '/', __FILE__ ) );
+
+if ( ! defined( 'NUCLIA_PATH' ) ) {
+	define( 'NUCLIA_PATH', __DIR__ . '/' );
+}
+
+/**
+ * Check for required PHP version.
+ *
+ * @since   1.0.0
+ *
+ * @return bool
+ */
+function nuclia_php_version_check() {
+	if ( version_compare( PHP_VERSION, NUCLIA_MIN_PHP_VERSION, '<' ) ) {
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Check for required WordPress version.
+ *
+ * @since   1.0.0
+ *
+ * @return bool
+ */
+function nuclia_wp_version_check() {
+	if ( version_compare( $GLOBALS['wp_version'], NUCLIA_MIN_WP_VERSION, '<' ) ) {
+		return false;
+	}
+	return true;
+}
+
+/**
+ * Admin notices if requirements aren't met.
+ *
+ * @since   1.0.0
+ */
+function nuclia_requirements_error_notice() {
+
+	$notices = [];
+
+	if ( ! nuclia_php_version_check() ) {
+		$notices[] = sprintf(
+			/* translators: placeholder 1 is minimum required PHP version, placeholder 2 is installed PHP version. */
+			esc_html__( 'Nuclia plugin requires PHP %1$s or higher. You’re still on %2$s.', 'wp-search-with-nuclia' ),
+			esc_html( NUCLIA_MIN_PHP_VERSION ),
+			esc_html( PHP_VERSION )
+		);
+	}
+
+	if ( ! nuclia_wp_version_check() ) {
+		$notices[] = sprintf(
+			/* translators: placeholder 1 is minimum required WordPress version, placeholder 2 is installed WordPress version. */
+			esc_html__( 'Nuclia plugin requires at least WordPress in version %1$s, You are on %2$s.', 'wp-search-with-nuclia' ),
+			esc_html( NUCLIA_MIN_WP_VERSION ),
+			esc_html( $GLOBALS['wp_version'] )
+		);
+	}
+
+	foreach ( $notices as $notice ) {
+		echo '<div class="notice notice-error"><p>' . esc_html( $notice ) . '</p></div>';
+	}
+}
+
+/**
+ * I18n.
+ *
+ * @since   1.0.0
+ */
+function nuclia_load_textdomain() {
+
+	$locale = apply_filters( 'plugin_locale', get_locale(), 'wp-search-with-nuclia' );
+
+	load_textdomain( 'wp-search-with-nuclia', WP_LANG_DIR . '/wp-search-with-nuclia/wp-search-with-nuclia-' . $locale . '.mo' );
+
+	load_plugin_textdomain( 'wp-search-with-nuclia', false, plugin_basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+
+add_action( 'init', 'nuclia_load_textdomain' );
+
+
+
+
+if ( nuclia_php_version_check() && nuclia_wp_version_check() ) {
+	
+	require_once NUCLIA_PATH . 'classmap.php';
+
+	$nuclia = Nuclia_Plugin_Factory::create();
+
+} else {
+	add_action( 'admin_notices', 'nuclia_requirements_error_notice' );
+}
